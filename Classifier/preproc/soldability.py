f90f9a4cd571c199ea0092b1b5c9f51568a47890
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2015-04-09 14:36:09 -0300

    Update soldability.py
    bugs fixed

--- Classifier/preproc/soldability.py
+++ Classifier/preproc/soldability.py
@@ -27,6 +27,8 @@
 #pattern_filename - padrao do nome das partes do arquivo
 #match_numeric - qual parte do padrao deve ser substituido pelo diferenciador da parte
 #list_index - lista de diferenciador das partes
+
+
 def read_file_parts(path, pattern_filename, match_numeric, list_index):
 	list_temp = []
 
@@ -58,6 +60,15 @@ def to_1st_column(linha_split, index_column):
 
 
 def sort(array):
+    global recursion_count
+    global million_recursion
+    recursion_count = recursion_count + 1
+
+    if (recursion_count == 1000000):
+        million_recursion = million_recursion + 1
+        print million_recursion, "million recursion"
+      	recursion_count = 0
+
     less = []
     equal = []
     greater = []
@@ -90,9 +101,11 @@ import sys
 
 sys.setrecursionlimit(1000000000)
 
-#init
+recursion_count = 0
+million_recursion = 0
 
-path = path =  "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[0:-2]) + "/Data/"
+#init
+path =  "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[0:-2]) + "/Data/"
 print "Loading BUYS data"
 linhas = read_single_file(path + "buys-proc-basico.dat")
 print len(linhas), "lines loaded"
@@ -104,9 +117,11 @@ linhas_item_1st_column = []
 
 
 ###ordenar as linhas por item para iterar em "blocos" de itens, evitar complexidade n2
+print "Formating buy data to sort by item"
 for linha in linhas:
 	linhas_item_1st_column.append(to_1st_column(linha.split(","), 5))
 
+print "Sorting buys"
 linhas_sort = sort(linhas_item_1st_column)
 linhas_item_1st_column = None
 ###
@@ -118,7 +133,7 @@ item_id_anterior = linhas_sort[0].split(",")[0]
 linhas_do_mesmo_item = []
 
 #apenas para poder registrar as linhas da ultima sessão
-linhas.append("-1,0,0,0,0.0,0,0")
+linhas_sort.append("-1,0,0,0,0.0,0,0")
 
 conta_linhas = 0
 
@@ -141,28 +156,30 @@ for linha in linhas_sort:
 
 
 	#apenas para dar print no andamento do script
-	if (conta_linhas * 10 % len(linhas_sort) == 0):
-		print "Processing Clicks ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "% done!"
+	if (conta_linhas * 100 % len(linhas_sort) == 0):
+		print "Processing Buys ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "%" + " done!"
 
 	elif ((len(linhas)/100) == conta_linhas):
-		print "Processing Buys ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "% done!"	
+		print "Processing Buys ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "%" + " done!"	
 
 print "All item boughts processed"
 print
 #Daqui para frente faz o mesmo com CLICKS
 
-path = path =  "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[0:-2]) + "/Data/"
+path =  "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[0:-2]) + "/Data/"
 print "Loading CLICKS data"
-linhas = read_single_file(path + "clicks_proc2.dat")
+linhas = read_file_parts(path, "clicks-proc-basico-parteX.dat", "X", [1,2,3,4,5,6])
 print len(linhas), "lines loaded"
 print
 
 linhas_item_1st_column = []
 
 ###ordenar as linhas por item para iterar em "blocos" de itens, evitar complexidade n2
+print "Formating click data to sort by item"
 for linha in linhas:
 	linhas_item_1st_column.append(to_1st_column(linha.split(","), 5))
 
+print "Sorting clicks"
 linhas_sort = sort(linhas_item_1st_column)
 linhas_item_1st_column = None
 ###
@@ -174,9 +191,10 @@ item_id_anterior = linhas_sort[0].split(",")[0]
 linhas_do_mesmo_item = []
 
 #apenas para poder registrar as linhas da ultima sessão
-linhas.append("-1,0,0,0,0.0,0,0")
+linhas_sort.append("-1,0,0,0,0.0,0,0")
 
 conta_linhas = 0
+percent_done = 0
 
 for linha in linhas_sort:
 	conta_linhas = conta_linhas + 1
@@ -195,11 +213,17 @@ for linha in linhas_sort:
 		linhas_do_mesmo_item.append(linha)
 
 	#apenas para dar print no andamento do script
-	if (conta_linhas * 10 % len(linhas_sort) == 0):
-		print "Processing Clicks ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "% done!"
+	if (len(linhas_sort) == conta_linhas):
+		percent_done = 100
+		conta_linhas = 0
+		#print datetime.datetime.now().time().hour, datetime.datetime.now().minute
+		print "Done:", (str(percent_done) + "%")
 
-	elif ((len(linhas)/100) == conta_linhas):
-		print "Processing Clicks ", str(((conta_linhas+0.0)/len(linhas_sort)) * 100) + "% done!"
+	elif ((len(linhas_sort) / 100) == conta_linhas):
+		percent_done = percent_done + 1
+		conta_linhas = 0
+		#print datetime.datetime.now().time().hour, datetime.datetime.now().minute
+		print "Done:", (str(percent_done) + "%")
 
 #agora monta a tabela item, clicados, vendidos
 #tabela dos items com venda
@@ -211,7 +235,7 @@ for item_b in buy_item_occur.keys():
 		buy_item_occur.pop(item_b, None)
 		click_item_occur.pop(item_b, None)
 
-	elif(not item_b in click_item_occur.keys()):
+	else:
 		arq_w.write(str(item_b) + "," + "ITEM_COMPRADO" + "," + "MAS_NAO_CLICADO" + "\n")
 
 #tabela dos items com click e sem venda
@@ -219,9 +243,24 @@ print "Processing items clicks"
 for item_c in click_item_occur.keys():
 	if(not item_c in buy_item_occur.keys()):
 		arq_w.write(str(item_c) + "," + str(click_item_occur[item_c]) + "," + "0" + "\n")
-		click_item_occur.pop(item_b, None)
+		click_item_occur.pop(item_c, None)
 
 #ao final ele testa se todos os items comprados foram clicados
 print
 print "WARNING"
 print "There is", len(buy_item_occur), "items boughts but not clicked"
+print "There is", len(click_item_occur), "items clicked but not saved"
+
+arq_ex = open(path + "sold_exceptions.dat", "w")
+
+for ex in click_item_occur:
+	arq_ex.write(ex + "\n")
+
+arq_ex.write("---")
+
+for ex in buy_item_occur:
+	arq_ex.write(ex + "\n")
+
+arq_ex.close()
+
+print "million_recursion", million_recursion
