e45d33a995d5cc7029b50bb18d9f78206310e6a2
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2015-05-17 20:41:47 -0300

    Update session-balanced--decision-tree-classification.R
    Refactoring script.
    Mains change: fread instead of read.csv

--- Classifier/session-balanced--decision-tree-classification.R
+++ Classifier/session-balanced--decision-tree-classification.R
@@ -1,4 +1,4 @@
-	###### get arguments from command line #####
+###### get arguments from command line #####
 options(echo=TRUE) # if you want see commands in output file
 args <- commandArgs(trailingOnly = TRUE)
 
@@ -26,28 +26,29 @@ path = args[4]
 
 #TRAIN PARTITION
 #IF YOU SET IT TO 100, THE TEST PARTITION WILL BE THE SAME AS TRAIN PARTITION
-train_partition_percent = as.integer(args[5])
+no.buys.proportion = as.integer(args[5])
 
 #TEST SIMULATION OR REAL TEST
 #TRUE IF YOU WANT TO GENERATE THE REAL TEST PREDICTION
 simulation = args[6]
 
 #EXAMPLE OF A TERMINAL COMMAND LINE CALLING
-#Rscript decision-tree-classification.R 1 1133 ss-da-mo-ti-it-sd-sc /local/dev/RecSys-cariris/ 100 TRUE
+#Rscript session-balanced--decision-tree-classification.R 5 1111 ss-da-mo-ti-bo-sd-sc /home/tales/dev/RecSys-cariris/ 1 TRUE
 ############################################
 
 library("gmodels")
 library("C50")
+library("data.table")
 
 clicks_source_path = paste(path, "/Data/clicks-proc-basico/", sep = "")
 
 print("Loading data")
-data = read.csv(paste(clicks_source_path, "clicks-proc-basico-parte1.dat", sep = ""), sep = ",", header = F)
-data = rbind(data, read.csv(paste(clicks_source_path, "clicks-proc-basico-parte2.dat", sep = ""), sep = ",", header = F))
-data = rbind(data, read.csv(paste(clicks_source_path, "clicks-proc-basico-parte3.dat", sep = ""), sep = ",", header = F))
-data = rbind(data, read.csv(paste(clicks_source_path, "clicks-proc-basico-parte4.dat", sep = ""), sep = ",", header = F))
-data = rbind(data, read.csv(paste(clicks_source_path, "clicks-proc-basico-parte5.dat", sep = ""), sep = ",", header = F))
-data = rbind(data, read.csv(paste(clicks_source_path, "clicks-proc-basico-parte6.dat", sep = ""), sep = ",", header = F))
+data = fread(paste(clicks_source_path, "clicks-proc-basico-parte1.dat", sep = ""), sep = ",", header = F)
+data = rbind(data, fread(paste(clicks_source_path, "clicks-proc-basico-parte2.dat", sep = ""), sep = ",", header = F))
+data = rbind(data, fread(paste(clicks_source_path, "clicks-proc-basico-parte3.dat", sep = ""), sep = ",", header = F))
+data = rbind(data, fread(paste(clicks_source_path, "clicks-proc-basico-parte4.dat", sep = ""), sep = ",", header = F))
+data = rbind(data, fread(paste(clicks_source_path, "clicks-proc-basico-parte5.dat", sep = ""), sep = ",", header = F))
+data = rbind(data, fread(paste(clicks_source_path, "clicks-proc-basico-parte6.dat", sep = ""), sep = ",", header = F))
 
 print(paste(nrow(data), "lines loaded"))
 print("\n")
@@ -102,7 +103,7 @@ if(!is.element("it", columns_array)){
   column_names = column_names[column_names != "ITEM"]
 }
 
-if(!is.element("ca", columns_array)){
+if(!is.element("ct", columns_array)){
   data$CATEGORY = NULL
   column_names = column_names[column_names != "CATEGORY"]
 }
@@ -115,35 +116,54 @@ head(data)
 if(is.element("wk", columns_array)){
   column_names[i] <- "WEEKDAY"
   i = i + 1
-  data = data.frame(data, WEEKDAY = read.csv(paste(path, "Data/columns/clicks-column-weekday.dat", sep = ""), sep = ",", header = F))
+  data = data.frame(data, WEEKDAY = fread(paste(path, "Data/columns/clicks-column-weekday.dat", sep = ""), sep = "\n", header = F))
+
 }
 
 if(is.element("cl", columns_array)){
   column_names[i] <- "CLICKED"
   i = i + 1
-  data = data.frame(data, CLICKED = read.csv(paste(path, "Data/columns/clicks-column-clicked.dat", sep = ""), sep = ",", header = F))
+  data = data.frame(data, CLICKED = fread(paste(path, "Data/columns/clicks-column-clicked.dat", sep = ""), sep = "\n", header = F))
 }
 
 if(is.element("bo", columns_array)){
   column_names[i] <- "BOUGHT"
   i = i + 1
-  data = data.frame(data, BOUGHT = read.csv(paste(path, "Data/columns/clicks-column-bought.dat", sep = ""), sep = ",", header = F))
+  data = data.frame(data, BOUGHT = fread(paste(path, "Data/columns/clicks-column-bought.dat", sep = ""), sep = "\n", header = F))
 }
 
 if(is.element("sd", columns_array)){
   column_names[i] <- "SOLDABILITY"
   i = i + 1
-  data = data.frame(data, SOLDABILITY = read.csv(paste(path, "Data/columns/clicks-column-soldability.dat", sep = ""), sep = ",", header = F))
+  data = data.frame(data, SOLDABILITY = fread(paste(path, "Data/columns/clicks-column-soldability.dat", sep = ""), sep = "\n", header = F))
 }
 
 if(is.element("sc", columns_array)){
   column_names[i] <- "SAME_CAT"
   i = i + 1
-  data = data.frame(data, SAME_CAT = read.csv(paste(path, "Data/columns/clicks-column-same-cat.dat", sep = ""), sep = ",", header = F))
+  data = data.frame(data, SAME_CAT = fread(paste(path, "Data/columns/clicks-column-same-cat.dat", sep = ""), sep = "\n", header = F))
+}
+
+if(is.element("mn", columns_array)){
+  column_names[i] <- "SOLD_MEAN"
+  i = i + 1
+  data = data.frame(data, SOLD_MEAN = fread(paste(path, "Data/columns/clicks-soldability_mean_by_session.dat", sep = ""), sep = "\n", header = F))
+}
+
+if(is.element("df", columns_array)){
+  column_names[i] <- "SOLD_MEAN_DIFF"
+  i = i + 1
+  data = data.frame(data, SOLD_MEAN_DIFF = fread(paste(path, "Data/columns/clicks-soldability_mean_diff_by_session.dat", sep = ""), sep = "\n", header = F))
+}
+
+if(is.element("md", columns_array)){
+  column_names[i] <- "SOLD_MEDIAN"
+  i = i + 1
+  data = data.frame(data, SOLD_MEAN_DIFF = fread(paste(path, "Data/columns/clicks-soldability_median_by_session.dat", sep = ""), sep = "\n", header = F))
 }
 
 column_names[i] <- "IS_BUY"
-data = data.frame(data, IS_BUY = read.csv(paste(path, "Data/columns/clicks-column-buy.dat", sep = ""), sep = ",", header = F))
+data = data.frame(data, IS_BUY = fread(paste(path, "Data/columns/clicks-column-buy.dat", sep = ""), sep = "\n", header = F))
 ###################################################
 print(column_names)
 print(column_names[!is.na(column_names)])
@@ -164,20 +184,23 @@ data.no.buys = NULL
 gc()
 
 
-#GENERATE data.balanced WITH SAME NUMBERS OF (SESSION WITH ANY BUY) AND (SESSIONS WITH NO BUY) 
+#GENERATE data.train WITH SAME NUMBERS OF (SESSION WITH ANY BUY) AND (SESSIONS WITH NO BUY) 
 data.no.buys = data[is.element(data$SESSION, no.buy.sessions),]
-no.buy.subset.sessions = sample(no.buy.sessions)[0:length(buy.sessions)]
+no.buy.subset.sessions = sample(no.buy.sessions)[0: ( no.buys.proportion * (length(buy.sessions)) ) ]
 data.no.buys.subset = data.no.buys[is.element(data.no.buys$SESSION, no.buy.subset.sessions),]
+nrow(data.no.buys.subset)
 
 data.buys = data[is.element(data$SESSION, buy.sessions),]
+nrow(data.buys)
 
-data.balanced = rbind(data.buys, data.no.buys.subset)
-sapply(data.balanced, class)
+data.train = rbind(data.buys, data.no.buys.subset)
+sapply(data.train, class)
 
 #Checar se a o subset de no.buy.sessions preservou o aspecto geral de data.no.buys
 #adicionar mais colunas a medida que o modelo cresce
 summary(data)
-summary(data.balanced)
+summary(data.train)
+data$MONTH = as.factor(data$MONTH)
 
 #liberando memoria
 data = NULL
@@ -194,33 +217,6 @@ if(!is.element("ss", columns_array)){
 }
 gc()
 
-### TRAIN AND TEST PARTITIONS ##
-set.seed(23456)
-n.data = nrow(data.balanced)
-print (paste("balanced dataset has", n.data, "lines"))
-print("\n")
-data.balanced <- data.balanced[order(runif(nrow(data.balanced))), ]
-
-train_partition_size = (train_partition_percent / 100) * n.data
-
-print (paste(train_partition_percent, "% of training means ", train_partition_size, " lines", sep = ""))
-print("\n")
-
-if(train_partition_percent == 100){
-	data.train = data.balanced
-	data.test = data.balanced
-	print("Model will be trained with 100% of balanced data.set")
-	print("\n")
-
-}else{
-	data.train = data.balanced[0:train_partition_size,]
-	data.test = data.balanced[train_partition_size + 1 : n.data,]
-	print(paste("Model will be trained with", train_partition_size, "% of balanced data.set"))
-	print(paste("Tests will be", 100 - train_partition_size, "% of balanced data.set"))
-	print(paste("It means", nrow(data.test), "lines of data"))
-	print("\n")
-}
-
 ################################
 
 error_cost <- matrix(c(aa, ab, ba, bb), nrow = 2)
@@ -231,39 +227,42 @@ head(data.train)
 
 print(paste("Column index to be used as classifier >", (is_buy_index * (-1)), "<"))
 
+#excluding CATEGORY due to missing values from train
+data.train$CATEGORY = NULL
+gc()
+
 model <- C5.0(data.train[is_buy_index], as.factor(data.train$IS_BUY), trials = n_trees, costs = error_cost)
 model
 #summary(model)
 
 #head(data.test)
-#head(data.balanced)
+#head(data.train)
 
 prediction.data.test <- predict(model, data.test)
 
 CrossTable(data.test$IS_BUY, prediction.data.test, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))
 
-output_filename = paste("session-based", "report", "-train-", as.character(train_partition_percent), "-", as.character(100 - train_partition_percent), "-", "forest", n_trees, "-", "costs", costs, "-", columns, sep = "")
+output_filename = paste("session-based-", "report", "-train-", as.character(train_partition_percent), "-", as.character(100 - train_partition_percent), "-", "forest", n_trees, "-", "costs", costs, "-", columns, sep = "")
 complete_path = paste(path, "/Classifier/reports/", output_filename, ".dat", sep = "")
 print(paste("Report saved as ", complete_path))
 print("\n")
-write(capture.output(CrossTable(data.test$IS_BUY, prediction.data.test, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))), complete_path)
+write(capture.output(CrossTable(data.train$IS_BUY, prediction.data.test, prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE, dnn = c('actual default', 'predicted default'))), complete_path)
 
 data.train = NULL
 data.test = NULL
-data.balanced = NULL
 prediction.data.test = NULL
 gc()
 
-if(simulation == "TRUE" & train_partition_percent == 100){
+if(simulation == "TRUE"){
 	print("Trained model will be used to predict real test")
 	print("\n")
 	print("Loading test data")
-	test = read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte1.dat", sep = ""), sep = ",", header = F)
-	test = rbind(test, read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte2.dat", sep = ""), sep = ",", header = F))
-	test = rbind(test, read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte3.dat", sep = ""), sep = ",", header = F))
-	test = rbind(test, read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte4.dat", sep = ""), sep = ",", header = F))
-	test = rbind(test, read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte5.dat", sep = ""), sep = ",", header = F))
-	test = rbind(test, read.csv(paste(path, "/Data/test-proc-basico/test-proc-basico-parte6.dat", sep = ""), sep = ",", header = F))
+	test = fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte1.dat", sep = ""), sep = "\n", header = F)
+	test = rbind(test, fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte2.dat", sep = ""), sep = "\n", header = F))
+	test = rbind(test, fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte3.dat", sep = ""), sep = "\n", header = F))
+	test = rbind(test, fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte4.dat", sep = ""), sep = "\n", header = F))
+	test = rbind(test, fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte5.dat", sep = ""), sep = "\n", header = F))
+	test = rbind(test, fread(paste(path, "/Data/test-proc-basico/test-proc-basico-parte6.dat", sep = ""), sep = "\n", header = F))
 	print("Test data loaded")
 	print("\n")
 
@@ -310,10 +309,11 @@ if(simulation == "TRUE" & train_partition_percent == 100){
 	  column_names = column_names[column_names != "TIME"]
 	}
 
-	if(!is.element("ca", columns_array)){
+	if(!is.element("ct", columns_array)){
 	  test$CATEGORY = NULL
 	  column_names = column_names[column_names != "CATEGORY"]
 	}
+	
 	###################################################
 	gc()
 
@@ -322,31 +322,49 @@ if(simulation == "TRUE" & train_partition_percent == 100){
 	if(is.element("wk", columns_array)){
 	  column_names[i] <- "WEEKDAY"
 	  i = i + 1
-	  test = data.frame(test, WEEKDAY = read.csv(paste(path, "Data/columns/test-column-weekday.dat", sep = ""), sep = ",", header = F))
+	  test = data.frame(test, WEEKDAY = fread(paste(path, "Data/columns/test-column-weekday.dat", sep = ""), sep = "\n", header = F))
 	}
 
 	if(is.element("cl", columns_array)){
 	  column_names[i] <- "CLICKED"
 	  i = i + 1
-	  test = data.frame(test, CLICKED = read.csv(paste(path, "Data/columns/test-column-clicked.dat", sep = ""), sep = ",", header = F))
+	  test = data.frame(test, CLICKED = fread(paste(path, "Data/columns/test-column-clicked.dat", sep = ""), sep = "\n", header = F))
 	}
 
 	if(is.element("bo", columns_array)){
 	  column_names[i] <- "BOUGHT"
 	  i = i + 1
-	  test = data.frame(test, BOUGHT = read.csv(paste(path, "Data/columns/test-column-bought.dat", sep = ""), sep = ",", header = F))
+	  test = data.frame(test, BOUGHT = fread(paste(path, "Data/columns/test-column-bought.dat", sep = ""), sep = "\n", header = F))
 	}
 
 	if(is.element("sd", columns_array)){
 	  column_names[i] <- "SOLDABILITY"
 	  i = i + 1
-	  test = data.frame(test, SOLDABILITY = read.csv(paste(path, "Data/columns/test-column-soldability.dat", sep = ""), sep = ",", header = F))
+	  test = data.frame(test, SOLDABILITY = fread(paste(path, "Data/columns/test-column-soldability.dat", sep = ""), sep = "\n", header = F))
 	}
 
 	if(is.element("sc", columns_array)){
 	  column_names[i] <- "SAME_CAT"
 	  i = i + 1
-	  test = data.frame(test, SAME_CAT = read.csv(paste(path, "Data/columns/test-column-same-cat.dat", sep = ""), sep = ",", header = F))
+	  test = data.frame(test, SAME_CAT = fread(paste(path, "Data/columns/test-column-same-cat.dat", sep = ""), sep = "\n", header = F))
+	}
+	
+	if(is.element("mn", columns_array)){
+	  column_names[i] <- "SOLD_MEAN"
+	  i = i + 1
+	  test = data.frame(test, SOLD_MEAN = fread(paste(path, "Data/columns/test-soldability_mean_by_session.dat", sep = ""), sep = "\n", header = F))
+	}
+	
+	if(is.element("df", columns_array)){
+	  column_names[i] <- "SOLD_MEAN_DIFF"
+	  i = i + 1
+	  test = data.frame(test, SOLD_MEAN_DIFF = fread(paste(path, "Data/columns/test-soldability_mean_diff_by_session.dat", sep = ""), sep = "\n", header = F))
+	}
+	
+	if(is.element("md", columns_array)){
+	  column_names[i] <- "SOLD_MEDIAN"
+	  i = i + 1
+	  test = data.frame(test, SOLD_MEDIAN = fread(paste(path, "Data/columns/test-soldability_median_by_session.dat", sep = ""), sep = "\n", header = F))
 	}
 	###################################################
 	print("Aditional columns loaded")
@@ -375,7 +393,7 @@ if(simulation == "TRUE" & train_partition_percent == 100){
 
 	test = test[test$PRED == 1,]
 
-	output_filename = paste("session-based", "forest", n_trees, "-", "costs", costs, "-", columns, sep = "")
+	output_filename = paste("session-based-", "forest", n_trees, "-", "costs", costs, "-", columns, sep = "")
 	complete_path = paste(path, "/Classifier/predicts/", output_filename, ".dat", sep = "")
 
 	print (paste("Saving predictions as ", complete_path))
